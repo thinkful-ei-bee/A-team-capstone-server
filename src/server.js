caab3fd65f44b0616b2a714034a1c79f87e71ae9
David Bolin <david@bolin.at>
2019-06-03 11:27:35 -0700

    added knex and set up server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
 'use strict';
+const knex = require('knex');
 const app = require('./app');
+const { PORT, DATABASE_URL } = require('./config');
 
-const { PORT } = require('./config');
+const db = knex({
+  client: 'pg',
+  connection: DATABASE_URL
+});
+
+app.set('db', db);
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
