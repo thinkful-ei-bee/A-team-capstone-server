f7eed2fbf78fcd822df34c7f9dc5ca195f847d78
Eddie Bueno <eddiebueno93@gmail.com>
2019-06-03 12:54:41 -0700

    seed file correctly seeds the database

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -6,41 +6,37 @@ TRUNCATE
   "projects",
   "users";
 
-INSERT INTO "users" ("id", "username", "email","user_description", "password","image")
+INSERT INTO "users" ("username", "email","user_description", "password","image")
 VALUES
   (
-    1,
     'admin',
     'admin@theateam.com',
+    'Professional in React, Node, and Express',
     -- password = "pass"
-    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
+    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG',
+    'https://www.gravatar.com/avatar/HASH'
   ),
   (
-    2,
     'sample_user',
     'sample_user@theateam.com',
+    'Proficent in Node and Express',
     -- password = "pass"
     -- need to implement hashed password
-    'pass'
-  )
+    'pass',
+    'https://www.gravatar.com/avatar/HASH'
+  );
 
-INSERT INTO "projects" ("id", "project_name", "owner_id","project_description")
+INSERT INTO "projects" ( "project_name", "owner_id","project_description")
 VALUES
-  (1, 'Spaced Repetition', 1,"Need someone to work on the backend.");
+  ('Spaced Repetition', 1,'Need someone to work on the backend.');
 
-INSERT INTO "bids" ("id", "user_id", "project_id", "bid")
+INSERT INTO "bids" ("user_id", "project_id", "bid")
 VALUES
-  (1, 2, 1, 'Bid')
+  (2, 1, 'Bid');
 
-INSERT INTO "usersProjectCollaboration" ("id", "project_id","collaborator_id","position")
+INSERT INTO "usersProjectCollaboration" ( "project_id","collaborator_id","position")
 VALUES 
-  (1,1,2,"First")
+  (1,2,'First');
 
--- because we explicitly set the id fields
--- update the sequencer for future automatic id setting
-SELECT setval('usersProjectCollaboration_id_seq', (SELECT MAX(id) from "usersProjectCollaboration"));
-SELECT setval('bids_id_seq', (SELECT MAX(id) from "bids"));
-SELECT setval('projects_id_seq', (SELECT MAX(id) from "projects"));
-SELECT setval('users_id_seq', (SELECT MAX(id) from "users"));
 
 COMMIT;
