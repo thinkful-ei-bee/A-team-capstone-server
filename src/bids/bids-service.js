bdc719a8ab7793888d9c05c7e446477f7c731c8c
David Bolin <david@bolin.at>
2019-06-03 14:11:24 -0700

    created bids service(no router yet)

---
+++ src/bids/bids-service.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const BidsService = {
+  addBid(db, newBid) {
+    return db
+      .insert(newBid)
+      .into('projects')
+      .returning('*')
+      .then(([bid]) => bid);
+  },
+  getBidsByUser(db, user_id) {
+    return db
+      .select('*')
+      .from('bids')
+      .where ( {user_id} );
+  },
+  getBidsByProject(db, project_id) {
+    return db
+      .select('*')
+      .from('bids')
+      .where( {project_id});
+  }
+};
+
+module.exports = BidsService;
