b1c724f1e5219f8b2afb8804a78636a06db49218
David Bolin <david@bolin.at>
2019-06-17 12:17:31 -0700

    instant update on comments should be functional

--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
 'use strict';
 const knex = require('knex');
 const app = require('./app');
+const http = require('http');
+const clients = require('./clients');
 const { PORT, DATABASE_URL } = require('./config');
+const server = http.createServer(app);
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws, req) => {
+  clients.addClient(ws, req);
+
+  ws.on('message', message => {
+    console.log('received: %s', message);
+    ws.send(`Hello, you sent -> ${message}`);
+    clients.clientList.forEach(ele => console.log(ele.username));
+  });
+
+  ws.send('Hi there, I am a WebSocket server');
+
+  ws.on('close', () => {
+    console.log('closed connection');
+  });
+});
 
 const db = knex({
   client: 'pg',
@@ -10,7 +31,7 @@ const db = knex({
 
 app.set('db', db);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server listening at http://localhost:${PORT}`);
 });
